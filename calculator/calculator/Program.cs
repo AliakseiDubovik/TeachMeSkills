a0d69f3f9bde36a4b8772c4a5a975709dddd5d9e
AliakseiDubovik <111907544+AliakseiDubovik@users.noreply.github.com>
2022-09-02 13:19:15 +0300

    completed the calculator

--- calculator/calculator/Program.cs
+++ calculator/calculator/Program.cs
@@ -6,11 +6,19 @@ namespace calculator
     {
         static void Main(string[] args)
         {
-            //PerformAdd();
-            //PerformSubtract();
-            //PerformMultiply();
-            //PerformDivide();
+            Console.WriteLine("Type a number, and press ENTER.: /n1 - Add /n 2 - Subtraction /n 3 - Multipliply /n 4 - Devide /n 5 - Percentage of the number");
+            Console.ReadKey();
+            PerformAdd();
+            PerformSubtract();
+            PerformMultiply();
+            PerformDivide();
             PerformPercentage();
+            bool repeat =  PerformDialogueUser();
+            
+            опистаь текстом пользователю, какую цифру ввести для того, чтобы выполнить определенную операцию
+            записать в  переменную значение ,которое пользователь ввел в ответе на вопрос
+                с помощью if проверяя переменную, в которую пользователь ввел значение вызвать метод выполняющую соответствующую операцию
+как объявить как присвоиить
 
 
         }
@@ -71,9 +79,23 @@ namespace calculator
             Console.WriteLine("Type a number, and then press Enter");
             var b = Convert.ToInt32(Console.ReadLine());
 
-            double c = a /100 * b;
+            double c = (double)a /100 * b;
             Console.WriteLine(c);
             Console.ReadKey();
+        }
+        static bool PerformDialogueUser()
+        {
+            Console.WriteLine("Wont to continue?");
+            string Y = Console.ReadLine();
+
+            if (Y == "Yes")
+            {
+
+                return true;
+                    }
+            return false;
+        
+        
         }
 
 
