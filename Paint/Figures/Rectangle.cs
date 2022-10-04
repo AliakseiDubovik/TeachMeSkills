767edf6eaec20ee36bbaac2c9c0205f0bd914379
AliakseiDubovik <111907544+AliakseiDubovik@users.noreply.github.com>
2022-10-04 22:51:04 +0300

    in process create rectangle

--- Paint/Figures/Rectangle.cs
+++ Paint/Figures/Rectangle.cs
@@ -4,14 +4,33 @@ using System.Text;
 
 namespace Figures
 {
-    //class Rectangle : Shape
-    //{
-    //    public static void MakeNewSquare()
-    //    {
+    class Rectangle : Shape
+    {
+        public static void MakeNewRectangle()
+        {
+            Console.Write("Enter \n width: \n height:");
+            int width = int.Parse(Console.ReadLine());
+            
+            Console.Write("Enter height:");
+            int height = int.Parse(Console.ReadLine());
 
-    //    }
+            var rectangle = new Rectangle(width, height);
+
+            rectangle.Print();
+        }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Rectangle(int width, int height)
+           
+
+        {
+            Width = width;
+            Height = height;
+
+        }
+        public override void Print()
 
 
 
-    //}
+    }
 }
